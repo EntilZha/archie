b36cb4d85c571ff5731fcaf5a6ed1538569b5155
Pedro Rodriguez <me@pedro.ai>
2020-06-27 18:15:03 -0400

    fix

--- archie/web.py
+++ archie/web.py
@@ -3,9 +3,11 @@ import arrow
 
 from sqlalchemy.orm import Session
 
-from fastapi import FastAPI, Depends, Body
+from fastapi import FastAPI, Depends
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+
+from pydantic import BaseModel
 
 from archie.database import (
     SessionLocal,
@@ -24,6 +26,10 @@ app = FastAPI()
 GOOGLE_SEARCH = "https://www.google.com/search?q=%s&btnK"
 GOOGLE_SUGGEST_FF = "https://www.google.com/complete/search?client=firefox&q=%s"
 GOOGLE_SUGGEST_OPERA = "https://www.google.com/complete/search?client=opera&q=%s"
+
+
+class Clip(BaseModel):
+    content: str
 
 
 def get_db():
@@ -101,8 +107,8 @@ async def paste(db: Session = Depends(get_db)):
 
 
 @app.post("/clip/copy")
-async def copy(content: str = Body(...), db: Session = Depends(get_db)):
-    add_clip(db, content)
+async def copy(content: Clip, db: Session = Depends(get_db)):
+    add_clip(db, content.content)
 
 
 @app.get("/clip/list")
